9d64d227d1e58749341ebdf6d32d9062162a5a1a
Felipe <felipe.f.araujo14@aluno.senai.br>
2026-06-16 16:55:46 -0300

    atualizado html e js

--- js/script_repeticoes.js
+++ js/script_repeticoes.js
@@ -1,6 +1,30 @@
-//Estrutura de repetiçao for
-const divfor = document.querySelector('div-for')
+// Estrutura de repetição FOR
+const divfor = document.querySelector('#div-for')
 
 for (let i = 0; i < 10; i++) {
-    console.log(i, "ola");
+    console.log(i, "ola")
+    divfor.innerHTML += `${i} - ola <br>`
 }
+
+// CONTADOR / ACUMULADOR
+
+const inputNum = document.querySelector('#num')
+const btnNum = document.querySelector('#btn-num')
+const divContAcum = document.querySelector('#div-cont-acum')
+
+let cont = 0
+let acum = 0
+
+btnNum.addEventListener('click', (evt) => {
+    let numDigitado = Number(inputNum.value)
+
+    cont++
+    acum += numDigitado
+
+    divContAcum.innerHTML = `
+        TOTAL DE NÚMEROS DIGITADOS: ${cont} <br>
+        A SOMA DOS NÚMEROS É: ${acum}
+    `
+
+    inputNum.value = ""
+})
